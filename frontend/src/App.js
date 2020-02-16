23f9f08c5412f3ec4906c3f961289156a04ec341
Enrique <enriquecardenas96@gmail.com>
2020-02-15 19:06:43 -0800

    add map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import LoginForm from './components/LoginForm'
+import tt from '@tomtom-international/web-sdk-maps';
+
+
 
 const App = () => {
   const [user, setUser] = useState(null)
 
+  useEffect(() => {
+     const script = document.createElement('script');
+     script.src = '../node_modules/@tomtom-international/web-sdk-maps/dist/maps-web.min.js';
+
+     document.body.appendChild(script);
+     script.async = true;
+     script.onload = function () {
+       tt.map({
+         key: 'AUrYekaUCt8px4UOktVSK2mcGzAypg81',
+         style: 'tomtom://vector/1/basic-main',
+         container: 'map',
+       });
+     }
+
+  }, [])
 
   return (
     <div>
@@ -11,6 +29,7 @@ const App = () => {
         <LoginForm /> :
         console.log('')
       }
+    <div id='map'></div>
     </div>
   );
 }
