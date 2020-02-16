2460611f4c457cfb5b9c9ef7e1b90784901a235b
Min Su Kim <kim97minsu@gmail.com>
2020-02-15 20:35:43 -0800

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Available from './components/Available'; // change this to App after ur done
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Available />, document.getElementById('root'));
