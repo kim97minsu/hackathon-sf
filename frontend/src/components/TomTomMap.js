9d458354c273805f22f5004ee796ad2ae21076fe
Enrique <enriquecardenas96@gmail.com>
2020-03-11 13:56:36 -0700

    fix map errors/warnings

--- frontend/src/components/TomTomMap.js
+++ frontend/src/components/TomTomMap.js
@@ -1,22 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import tt from '@tomtom-international/web-sdk-maps';
-
 
 const TomTomMap = () => {
 
   useEffect(() => {
-     const script = document.createElement('script');
-     script.src = 'node_modules/@tomtom-international/web-sdk-maps/dist/maps-web.min.js';
-
-     document.body.appendChild(script);
-     script.async = true;
-     script.onload = function () {
-       tt.map({
-         key: 'AUrYekaUCt8px4UOktVSK2mcGzAypg81',
-         style: 'tomtom://vector/1/basic-main',
-         container: 'map',
-       });
-     }
+    const script = document.createElement('script');
+    document.body.appendChild(script);
+    script.async = true;
+    tt.map({
+      key: 'AUrYekaUCt8px4UOktVSK2mcGzAypg81',
+      style: 'tomtom://vector/1/basic-main',
+      container: 'map'
+    }); 
 
   }, [])
 
